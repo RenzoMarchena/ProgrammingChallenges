4b8c9c968897939aeb11752c7117d96be513e4d4
Renzo Stanislav Marchena Sekli <marchena_sekli@hotmail.com>
2017-02-23 17:19:59 -0500

    Major Upgrade

---
+++ Searchfight/SearchFight.Tests/SearchResultsAnalyzerTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SearchFight.Interfaces;
+using System.Linq;
+
+
+namespace SearchFight.Tests
+{
+    [TestClass()]
+    public class SearchResultsAnalyzerTests
+    {
+        [TestMethod()]
+        public void GetResultsByProgrammingLanguageTest()
+        {
+            var sra = new SearchResultsAnalyzer();
+            var results = sra.GetResultsByProgrammingLanguage
+                                                (new SearchResult[]
+                                                    {
+                                                     new SearchResult { Query =".net",
+                                                                        SearchEngineUsed ="Google",
+                                                                        NumberOfResults = 1900000 },
+                                                     new SearchResult { Query ="java",
+                                                                        SearchEngineUsed ="Google",
+                                                                        NumberOfResults = 2900000 },
+                                                     new SearchResult { Query =".net",
+                                                                        SearchEngineUsed ="MSN Search",
+                                                                        NumberOfResults = 19001200 },
+                                                     new SearchResult { Query ="java",
+                                                                        SearchEngineUsed ="MSN Search",
+                                                                        NumberOfResults = 800000 }
+                                                     }
+                                                 );
+
+            Assert.IsTrue(results.Count() == 2);
+            Assert.IsTrue(results.Intersect(new string[] { ".net: Google: 1900000 MSN Search: 19001200 ",
+                                                          "java: Google: 2900000 MSN Search: 800000 " }).Count() == 2);
+        }
+
+        [TestMethod()]
+        public void GetWinnerBySearchEngineTest()
+        {
+            var sra = new SearchResultsAnalyzer();
+            var winners = sra.GetWinnerBySearchEngine
+                                                (new SearchResult[]
+                                                    {
+                                                     new SearchResult { Query =".net",
+                                                                        SearchEngineUsed ="Google",
+                                                                        NumberOfResults = 1900000 },
+                                                     new SearchResult { Query ="java",
+                                                                        SearchEngineUsed ="Google",
+                                                                        NumberOfResults = 2900000 },
+                                                     new SearchResult { Query =".net",
+                                                                        SearchEngineUsed ="MSN Search",
+                                                                        NumberOfResults = 19001200 },
+                                                     new SearchResult { Query ="java",
+                                                                        SearchEngineUsed ="MSN Search",
+                                                                        NumberOfResults = 800000 }
+                                                     }
+                                                 );
+
+            Assert.IsTrue(winners.Count() == 2);
+            Assert.IsTrue(winners.Intersect(new string[] { "Google winner: java",
+                                                          "MSN Search winner: .net" }).Count() == 2);
+
+
+
+        }
+    }
+}
---
+++ Searchfight/Searchfight/ArgumentsParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SearchFight
+{
+    public class ArgumentsParser
+    {
+        public static IEnumerable<string> ParseArguments(string[] args)
+        {
+
+            var programmingLanguages = new List<string>();
+
+            // Join the arguments array to split it in a more useful way
+            var arguments = String.Join(" ", args);
+
+            // Extract Programming Languages that are between quotation marks to allow spaces 
+            var pattern = "\"[^\"]*\"";
+
+            foreach (var programmingLanguage in Regex.Matches(arguments, pattern))
+            {
+                programmingLanguages.Add(programmingLanguage.ToString().Replace('"', ' '));
+                arguments = arguments.Replace(programmingLanguage.ToString(), "");
+            }
+
+            //Extract the other Programming Languages
+            string[] otherProgrammingLanguages = arguments.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var programmingLanguage in otherProgrammingLanguages)
+                programmingLanguages.Add(programmingLanguage);
+
+            return programmingLanguages;
+
+        }
+    }
+}
