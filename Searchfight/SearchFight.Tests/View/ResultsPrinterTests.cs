2f831db2be7d8a6b0b3305a732ae853b7345b2f0
Renzo Stanislav Marchena Sekli <marchena_sekli@hotmail.com>
2017-02-27 09:39:17 -0500

    Major Upgrade

--- Searchfight/SearchFight.Tests/View/ResultsPrinterTests.cs
+++ Searchfight/SearchFight.Tests/View/ResultsPrinterTests.cs
@@ -1,34 +1,36 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SearchFight.Interfaces;
+using SearchFight.View;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-namespace SearchFight.Tests
+namespace SearchFight.View.Tests
 {
     [TestClass()]
-    public class SearchResultsAnalyzerTests
+    public class ResultsPrinterTests
     {
         [TestMethod()]
         public void GetResultsByProgrammingLanguageTest()
         {
-            var sra = new SearchResultsAnalyzer();
-            var results = sra.GetResultsByProgrammingLanguage
-                                                (new SearchResult[]
+            var searchResults = new Model.SearchResult[]
                                                     {
-                                                     new SearchResult { Query =".net",
+                                                     new Model.SearchResult { Query =".net",
                                                                         SearchEngineUsed ="Google",
                                                                         NumberOfResults = 1900000 },
-                                                     new SearchResult { Query ="java",
+                                                     new Model.SearchResult { Query ="java",
                                                                         SearchEngineUsed ="Google",
                                                                         NumberOfResults = 2900000 },
-                                                     new SearchResult { Query =".net",
+                                                     new Model.SearchResult { Query =".net",
                                                                         SearchEngineUsed ="MSN Search",
                                                                         NumberOfResults = 19001200 },
-                                                     new SearchResult { Query ="java",
+                                                     new Model.SearchResult { Query ="java",
                                                                         SearchEngineUsed ="MSN Search",
                                                                         NumberOfResults = 800000 }
-                                                     }
-                                                 );
+                                                     };
+
+            var results = ResultsPrinter.GetResultsByProgrammingLanguage(searchResults);
 
             Assert.IsTrue(results.Count() == 2);
             Assert.IsTrue(results.Intersect(new string[] { ".net: Google: 1900000 MSN Search: 19001200 ",
@@ -38,32 +40,28 @@ namespace SearchFight.Tests
         [TestMethod()]
         public void GetWinnerBySearchEngineTest()
         {
-            var sra = new SearchResultsAnalyzer();
-            var winners = sra.GetWinnerBySearchEngine
-                                                (new SearchResult[]
+            var searchResults = new Model.SearchResult[]
                                                     {
-                                                     new SearchResult { Query =".net",
+                                                     new Model.SearchResult { Query =".net",
                                                                         SearchEngineUsed ="Google",
                                                                         NumberOfResults = 1900000 },
-                                                     new SearchResult { Query ="java",
+                                                     new Model.SearchResult { Query ="java",
                                                                         SearchEngineUsed ="Google",
                                                                         NumberOfResults = 2900000 },
-                                                     new SearchResult { Query =".net",
+                                                     new Model.SearchResult { Query =".net",
                                                                         SearchEngineUsed ="MSN Search",
                                                                         NumberOfResults = 19001200 },
-                                                     new SearchResult { Query ="java",
+                                                     new Model.SearchResult { Query ="java",
                                                                         SearchEngineUsed ="MSN Search",
                                                                         NumberOfResults = 800000 }
-                                                     }
-                                                 );
+                                                     };
+            var winners = ResultsPrinter.GetWinnerBySearchEngine(searchResults);
+                                         
 
             Assert.IsTrue(winners.Count() == 2);
             Assert.IsTrue(winners.Intersect(new string[] { "Google winner: java",
                                                           "MSN Search winner: .net" }).Count() == 2);
 
-
-
         }
-
     }
 }
