c476850e67f14309c87ec040c0513b7546029285
Renzo Stanislav Marchena Sekli <marchena_sekli@hotmail.com>
2017-03-01 23:10:50 -0500

    Major Upgrade

---
+++ Searchfight/SearchFight.Dependencies/SearchFightContainer.cs
@@ -0,0 +1,25 @@
+﻿using SearchFight.Interfaces;
+using SearchFight.Implementations;
+using SearchFight.Implementations.SearchEngines;
+using SimpleInjector;
+
+namespace SearchFight.Dependencies
+{ 
+    public class SearchFightContainer
+    {
+        private static Container container; 
+        public static Container BuildContainer()
+        {
+            container = new Container();
+
+            container.Register<IQueryMaker, QueryMaker>(Lifestyle.Singleton);
+            container.RegisterCollection<ISearchEngine>(new[] { typeof(Google), typeof(MSNSearch) });
+            container.Register<IHttpHandler, HttpHandler>(Lifestyle.Singleton);
+
+            container.Verify();
+
+            return container;
+        }
+
+    }
+}
